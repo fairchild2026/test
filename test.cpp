ecc06d9413e78c5dd3ea48fa3dd518ec135bb06f
top13 <xiantzhu@geogle.com>
2026-08-30 21:14:59 +0800

    - << 这一行

--- test.cpp
+++ test.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 int main() {   
    using namespace std; 
-   cout << "Come up and C++ me some time.";  
-   cout << endl; 
+   cout << "Come up and C++ me some time." << endl;
    cout << "You won't regret it!" << endl;
    /*cin.get();*/
    return 0; 
